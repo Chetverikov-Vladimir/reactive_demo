abb9d56a78df283639e89d121e94017a346850f2
Chetverikov-Vladimir <vladimir.che@outlook.com>
2021-06-06 22:32:41 +0300

    Add SimpleThreadRun class

---
+++ multithreading/src/test/java/threads/SimpleThreadRun.java
@@ -0,0 +1,31 @@
+package threads;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleThreadRun {
+
+    /**
+     * Запуск нового потока путем наследования от Thread
+     */
+    @Test
+    public void threadExtends() {
+        Thread sampleThread = new SampleThread();
+        sampleThread.start();
+    }
+
+    /**
+     * Запуск нового потока путем имплементации интерфейса Runnable
+     */
+    @Test
+    public void runnableImplements() {
+        Runnable task = () -> System.out.println("Print from  " + this.getClass().getSimpleName() + " thread " + Thread.currentThread());
+        Thread runnableThread = new Thread(task);
+        runnableThread.start();
+    }
+
+    class SampleThread extends Thread {
+        public void run() {
+            System.out.println("Print from  " + this.getClass().getSimpleName() + " thread " + Thread.currentThread());
+        }
+    }
+}
